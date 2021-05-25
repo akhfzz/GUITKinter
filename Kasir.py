c9fe5b7824a0de1cee17ea9a63de3c00dea01b2e
Akhmad Faizal <73004952+akhfzz@users.noreply.github.com>
2021-05-25 13:22:12 +0700

    Update Kasir.py

--- Kasir.py
+++ Kasir.py
@@ -6,68 +6,71 @@ class Kasir(object):
 		self.nm_barang = nm_barang
 		self.harga = harga
 		self.Qty = Qty
+		
+class Kasir:
+	def __init__(self, master):
+		self.master = master
+		master.title('Kasir Toko Kelontong')
+		master.geometry('450x300+35+35')
+		self.dataList = []
 
-master= Tk()
-master.title('Kasir Toko Kelontong')
-master.geometry('450x300+35+35')
-dataList = []
+		self.brg = ''
+		self.qty = 0
+		self.harga = 0
 
-brg = ''
-qty = 0
-harga = 0
+		def validation(valid):
+			if not valid:
+				self.brg = ''
+				self.qty = 0
+				self.harga = 0
+			try:
+				self.brg = str(valid)
+				self.qty = int(valid)
+				self.harga = int(valid)
+				return True
+			except ValueError:
+				return False
 
-def validation(valid):
-	if not valid:
-		brg = ''
-		qty = 0
-		harga = 0
-	try:
-		brg = str(valid)
-		qty = int(valid)
-		harga = int(valid)
-		return True
-	except ValueError:
-		return False
+		valid = master.register(self.validation)
 
-valid = master.register(validation)
+		self.Lb_barang = Label(self.master, text='Nama Barang : ').grid(row=1, column=0, padx=20, columnspan=6,  pady=10)
+		self.EnBarang = Entry(self.master, width=40, validate='focus', validatecommand=(valid, '%P'))
+		self.EnBarang.grid(row=1, column=6, columnspan=8, pady=10)
 
-Lb_barang = Label(master, text='Nama Barang : ').grid(row=1, column=0, padx=20, columnspan=6,  pady=10)
-EnBarang = Entry(master, width=40, validate='focus', validatecommand=(valid, '%P'))
-EnBarang.grid(row=1, column=6, columnspan=8, pady=10)
+		self.Lb_harga = Label(self.master, text='Harga Barang : ').grid(row=2, column=0, padx=20, columnspan=6,pady=10)
+		self.EnHarga = self.Entry(master, width=40,validate='focus', validatecommand=(valid, '%P'))
+		self.EnHarga.grid(row=2, column=6, columnspan=8, pady=10)
 
-Lb_harga = Label(master, text='Harga Barang : ').grid(row=2, column=0, padx=20, columnspan=6,pady=10)
-EnHarga = Entry(master, width=40,validate='focus', validatecommand=(valid, '%P'))
-EnHarga.grid(row=2, column=6, columnspan=8, pady=10)
+		self.Lb_jumlah = Label(self.master, text='QTY : ').grid(row=3, column=0, padx=20, columnspan=6, pady=10)
+		self.EnJumlah = Entry(self.master, width=40, validate='focus', validatecommand=(valid, '%P'))
+		self.EnJumlah.grid(row=3, column=6, columnspan=8,pady=10)
 
-Lb_jumlah = Label(master, text='QTY : ').grid(row=3, column=0, padx=20, columnspan=6, pady=10)
-EnJumlah = Entry(master, width=40, validate='focus', validatecommand=(valid, '%P'))
-EnJumlah.grid(row=3, column=6, columnspan=8,pady=10)
+		def Add(ket):
+			if ket == 'tambah':
+				i = 0
+				self.listHarga = []
+				self.listJumlah = []
+				for index in range(0, len(self.dataList)+1):
+					i += 1
+					self.dataList.append(Kasir(i, self.EnBarang.get(), self.EnHarga.get(), self.EnJumlah.get()))
+					Label(self.master, text=f'{i}').grid(row=i+10,column=0,  pady=10,columnspan=3)
+					Label(self.master, text=f'{self.dataList[index].nm_barang}').grid(row=i+10,column=3,  pady=10,columnspan=3)
+					Label(self.master, text=f'{int(dataList[index].harga) * int(self.dataList[index].Qty)}').grid(row=i+10,column=11,  pady=10,columnspan=3)
+					Label(self.master, text=f'{self.dataList[index].Qty}').grid(row=i+10,column=7,  pady=10,columnspan=3)
+					self.listHarga.append(int(self.dataList[index].harga))
+					self.listJumlah.append(int(self.dataList[index].Qty))
+					h = 0
+					for x in range(0, len(self.listJumlah)):
+						h += self.listHarga[x] * self.listJumlah[x]
+						Label(self.master, text='Payment',borderwidth=1,relief='raised', width=20 ).grid(row=30, column=5, padx=20, columnspan=6, pady=10)
+						Label(self.master,text=f'{h}').grid(row=30,column=11,  pady=20,columnspan=3)
 
-def Add(ket):
-	if ket == 'tambah':
-		i = 0
-		listHarga = []
-		listJumlah = []
-		for index in range(0, len(dataList)+1):
-			i += 1
-			dataList.append(Kasir(i, EnBarang.get(), EnHarga.get(), EnJumlah.get()))
-			Label(master, text=f'{i}').grid(row=i+10,column=0,  pady=10,columnspan=3)
-			Label(master, text=f'{dataList[index].nm_barang}').grid(row=i+10,column=3,  pady=10,columnspan=3)
-			Label(master, text=f'{int(dataList[index].harga) * int(dataList[index].Qty)}').grid(row=i+10,column=11,  pady=10,columnspan=3)
-			Label(master, text=f'{dataList[index].Qty}').grid(row=i+10,column=7,  pady=10,columnspan=3)
-			listHarga.append(int(dataList[index].harga))
-			listJumlah.append(int(dataList[index].Qty))
-			h = 0
-			for x in range(0, len(listJumlah)):
-				h += listHarga[x] * listJumlah[x]
-				Label(master, text='Payment',borderwidth=1,relief='raised', width=20 ).grid(row=30, column=5, padx=20, columnspan=6, pady=10)
-				Label(master,text=f'{h}').grid(row=30,column=11,  pady=20,columnspan=3)
+		btn_add = Button(self.master, text='Add', command=lambda:self.Add('tambah'), bg='red', fg='white').grid(row=4, column=11, columnspan=4)
 
-btn_add = Button(master, text='Add', command=lambda:Add('tambah'), bg='red', fg='white').grid(row=4, column=11, columnspan=4)
-
-no_brg = Label(master, text='No', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=0, pady=20,columnspan=3)
-nm_brg = Label(master, text='Nama Barang', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=3, pady=20,columnspan=4)
-Qty = Label(master, text='Qty', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=7,pady=20,columnspan=4)
-Total = Label(master, text='Total', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=11,pady=20,columnspan=4)
-
-master.mainloop()
+		no_brg = Label(self.master, text='No', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=0, pady=20,columnspan=3)
+		nm_brg = Label(self.master, text='Nama Barang', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=3, pady=20,columnspan=4)
+		Qty = Label(self.master, text='Qty', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=7,pady=20,columnspan=4)
+root = Tk()
+obj = Kasir(root)
+root.mainloop()
+		Total = Label(self.master, text='Total', borderwidth=1, width=20, relief='raised', bg='black', fg='white').grid(row=10, column=11,pady=20,columnspan=4)
